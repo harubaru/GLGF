296eb82c402472c4f2f8e81a29d711ff7bfebfa2
tone121 <tone121@cox.net>
2019-02-13 21:34:06 -0700

    improved audio

--- inc/audio.h
+++ inc/audio.h
@@ -3,8 +3,11 @@
 
 #include <SDL2/SDL_mixer.h>
 
-void audio_init(char **audiofiles);
-void audio_play(int i, int loops);
+void audio_init(void);
+void audio_load_music(char **files, size_t count);
+void audio_load_chunk(char **files, size_t count);
+void audio_playmusic(size_t i, int loops);
+void audio_playchunk(size_t i, int loops);
 void audio_kill(void);
 
 #endif
--- src/audio.c
+++ src/audio.c
@@ -1,34 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <audio.h>
 
-Mix_Music *sample[2];
+static size_t music_count = 0;
+static Mix_Music **music;
 
-void audio_init(char **audiofiles)
+static size_t chunk_count = 0;
+static Mix_Chunk **chunk;
+
+void audio_init(void)
 {
-	if(Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 512)) {
+	if(Mix_OpenAudio(48000, AUDIO_F32SYS, 2, 2048)) {
 		fprintf(stderr, "%s\n", SDL_GetError());
 		return;
 	}
-	if (Mix_AllocateChannels(2)) {
-		fprintf(stderr, "%s\n", SDL_GetError());
-	}
 
-	for (int i = 0; i < 2; i++) {
-		sample[i] = Mix_LoadOGG(audiofiles[i]);
-		if (!sample[i])
-			fprintf(stderr, "failed to load %s\n", audiofiles[i]);
+	if (Mix_AllocateChannels(2))
+		fprintf(stderr, "%s\n", SDL_GetError());
+}
+
+void audio_load_music(char **files, size_t count)
+{
+	music_count = count;
+	music = calloc(count, sizeof(Mix_Music *));
+
+	for (size_t i = 0; i < count; i++) {
+		music[i] = Mix_LoadMUS(files[i]);
+		if (!music[i])
+			fprintf(stderr, "Failed to load audio - %s\n", files[i]);
 	}
 }
 
-void audio_play(int i, int loops)
+void audio_load_chunk(char **files, size_t count)
 {
-	Mix_PlayMusic(sample[i], loops);
+	chunk_count = count;
+	chunk = calloc(count, sizeof(Mix_Chunk *));
+
+	for (size_t i = 0; i < count; i++) {
+		chunk[i] = Mix_LoadWAV(files[i]);
+		if (!chunk[i])
+			fprintf(stderr, "Failed to load audio - %s\n", files[i]);
+	}
+}
+
+void audio_playmusic(size_t i, int loops)
+{
+	if (i > music_count)
+		return;
+
+	Mix_PlayMusic(music[i], loops);
+}
+
+void audio_playchunk(size_t i, int loops)
+{
+	if (i > chunk_count)
+		return;
+
+	Mix_PlayChannel(-1, chunk[i], loops);
 }
 
 void audio_kill(void)
 {
-	for (int i = 0; i < 2; i++)
-		Mix_FreeMusic(sample[i]);
+	size_t i;
+
+	for (i = 0; i < music_count; i++)
+		Mix_FreeMusic(music[i]);
+	for (i = 0; i < chunk_count; i++)
+		Mix_FreeChunk(chunk[i]);
 
 	Mix_CloseAudio();
 }
