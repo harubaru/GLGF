6ef4db3d9e331287f493db1e9ab6c434f85d5291
tone121 <tone121@cox.net>
2019-02-17 23:03:15 -0700

    vectorize aabb

--- src/aabb.c
+++ src/aabb.c
@@ -2,26 +2,28 @@
 
 _Bool aabb_point_intersect(vec2 point, vec2 bp, vec2 bs)
 {
-	float xmin = bp[0];
-	float xmax = bp[0] + bs[0];
-	float ymin = bp[1];
-	float ymax = bp[1] + bs[1];
+	vec2 min, max;
 
-	return (point[0] >= xmin && point[0] <= xmax) && (point[1] >= ymin && point[1] <= ymax);
+	for (int i = 0; i < 2; i++) {
+		min[i] = bp[i];
+		max[i] = bp[i] + bs[i];
+	}
+
+	return (point[0] >= min[0] && point[0] <= max[0]) && (point[1] >= min[1] && point[1] <= max[1]);
 }
 
 _Bool aabb_quad_intersect(vec2 bp1, vec2 bs1, vec2 bp2, vec2 bs2)
 {
-	float xmin1 = bp1[0];
-	float xmax1 = bp1[0] + bs1[0];
-	float ymin1 = bp1[1];
-	float ymax1 = bp1[1] + bs1[1];
+	vec2 min1, max1, min2, max2;
 
-	float xmin2 = bp2[0];
-	float xmax2 = bp2[0] + bs2[0];
-	float ymin2 = bp2[1];
-	float ymax2 = bp2[1] + bs2[1];
+	for (int i = 0; i < 2; i++) {
+		min1[i] = bp1[i];
+		max1[i] = bp1[i] + bs1[i];
+	}
+	for (int i = 0; i < 2; i++) {
+		min2[i] = bp2[i];
+		max2[i] = bp2[i] + bs2[i];
+	}
 
-
-	return (xmin1 <= xmax2 && xmax1 >= xmin2) && (ymin1 <= ymax2 && ymax1 >= ymin2);
+	return (min1[0] <= max2[0] && max1[0] >= min2[0]) && (min1[1] <= max2[1] && max1[1] >= min2[1]);
 }
