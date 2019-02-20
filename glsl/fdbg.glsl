f95cc8cbde005dc3e0da1e0eec66b3ef113abc3c
tone121 <tone121@cox.net>
2019-02-19 18:30:18 -0700

    made sprite shader interact with bloom shader

--- glsl/fdbg.glsl
+++ glsl/fdbg.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 
-out vec4 Fragment;
+layout (location = 0) out vec4 Fragment;
+layout (location = 1) out vec4 Fragment2;
 
 in VS_OUT {
 	vec2 TexCoord;
@@ -20,4 +21,11 @@ void main()
 
 	if (Fragment.a < 0.05)
 		discard;
+
+	float linear_brightness = dot(Fragment.rgb, vec3(0.2126, 0.7152, 0.0722));
+	if (linear_brightness > 1.0) {
+		Fragment2 = vec4(Fragment.rgb, 1.0);
+	} else {
+		Fragment2 = vec4(vec3(0.0), 1.0);
+	}
 }
