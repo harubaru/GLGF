a246038581054958cb082b7a7276eb9248fcd73f
tone121 <tone121@cox.net>
2019-02-20 19:40:24 -0700

    added functions to help with shadow rendering

--- inc/light.h
+++ inc/light.h
@@ -5,17 +5,21 @@
 #include <gl3w.h>
 #include <shader.h>
 #include <sprite.h>
+#include <postprocessing.h>
 
 typedef struct light {
 	vec2 position;
+	vec2 normal;
 	vec2 size;
 	vec3 color;
 } light_t;
 
 light_t light_create(vec2 position, vec2 size, vec3 color);
-void light_init(void);
+void light_init(int w, int h);
 void light_bind(void);
 void light_draw(light_t light);
+void light_draw_centered(light_t light);
+void light_scene_draw_offset(light_t light, vec2 offset);
 void light_unbind(void);
 
 #endif
--- src/light.c
+++ src/light.c
@@ -1,5 +1,6 @@
 #include <light.h>
 
+static int width, height;
 static GLuint shader;
 
 light_t light_create(vec2 position, vec2 size, vec3 color)
@@ -14,12 +15,17 @@ light_t light_create(vec2 position, vec2 size, vec3 color)
 
 	for (int i = 0; i < 3; i++)
 		ret.color[i] = color[i];
+	
+	for (int i = 0; i < 2; i++)
+		ret.normal[i] = (size[i] / 2);
 
 	return ret;
 }
 
-void light_init(void)
+void light_init(int w, int h)
 {
+	width = w;
+	height = h;
 	shader = shader_binary_cache("./shaders/cached/light.glsp", "./shaders/vdbg.glsl", "./shaders/flight.glsl");
 }
 
@@ -32,6 +38,24 @@ void light_draw(light_t light)
 {
 	glUniform3f(glGetUniformLocation(shader, "color"), light.color[0], light.color[1], light.color[2]);
 	sprite_draw(0, shader, light.position, light.size);
+}
+
+void light_draw_centered(light_t light)
+{
+	vec2 light_pos_offset = { (width/2) - light.normal[0], (height/2) - light.normal[1] };
+	glUniform3f(glGetUniformLocation(shader, "color"), light.color[0], light.color[1], light.color[2]);
+	sprite_draw(0, shader, light_pos_offset, light.size);
+}
+
+void light_scene_draw_offset(light_t light, vec2 offset)
+{
+	if (!offset)
+		return;
+
+	vec2 light_pos_offset = { (width/2) - light.normal[0], (height/2) - light.normal[1] };
+	
+	for (int i = 0; i < 2; i++)
+		offset[i] = light_pos_offset[i] - light.position[i];
 }
 
 void light_unbind(void)
