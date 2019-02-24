4b054df60df93923b6fc49dada2b47694576d905
tone121 <tone121@cox.net>
2019-02-24 12:00:11 -0700

    made aabb functions use aabb struct

--- inc/aabb.h
+++ inc/aabb.h
@@ -10,9 +10,9 @@ typedef struct aabb {
 
 aabb_t aabb_gen_quad(vec2 bp, vec2 bs);
 
-_Bool aabb_point_intersect(vec2 point, vec2 bp, vec2 bs);
-_Bool aabb_quad_intersect(vec2 bp1, vec2 bs1, vec2 bp2, vec2 bs2);
-float aabb_ray_distance(vec2 rp, vec2 rd, vec2 bp, vec2 bs);
-_Bool aabb_ray_intersect(float *distance, vec2 rp, vec2 rd, vec2 bp, vec2 bs);
+_Bool aabb_point_intersect(vec2 point, aabb_t a);
+_Bool aabb_quad_intersect(aabb_t a, aabb_t b);
+_Bool aabb_ray_intersect(float *distance, vec2 rp, vec2 rd, aabb_t a);
+float aabb_ray_distance(vec2 rp, vec2 rd, aabb_t a);
 
 #endif
--- src/aabb.c
+++ src/aabb.c
@@ -12,50 +12,26 @@ aabb_t aabb_gen_quad(vec2 bp, vec2 bs)
 	return aabb;
 }
 
-_Bool aabb_point_intersect(vec2 point, vec2 bp, vec2 bs)
+_Bool aabb_point_intersect(vec2 point, aabb_t a)
 {
-	vec2 min, max;
-
-	for (int i = 0; i < 2; i++) {
-		min[i] = bp[i];
-		max[i] = bp[i] + bs[i];
-	}
-
-	return (point[0] >= min[0] && point[0] <= max[0]) && (point[1] >= min[1] && point[1] <= max[1]);
+	return (point[0] >= a.min[0] && point[0] <= a.max[0]) && (point[1] >= a.min[1] && point[1] <= a.max[1]);
 }
 
-_Bool aabb_quad_intersect(vec2 bp1, vec2 bs1, vec2 bp2, vec2 bs2)
+_Bool aabb_quad_intersect(aabb_t a, aabb_t b)
 {
-	vec2 min1, max1, min2, max2;
-
-	for (int i = 0; i < 2; i++) {
-		min1[i] = bp1[i];
-		max1[i] = bp1[i] + bs1[i];
-	}
-	for (int i = 0; i < 2; i++) {
-		min2[i] = bp2[i];
-		max2[i] = bp2[i] + bs2[i];
-	}
-
-	return (min1[0] <= max2[0] && max1[0] >= min2[0]) && (min1[1] <= max2[1] && max1[1] >= min2[1]);
+	return (a.min[0] <= b.max[0] && a.max[0] >= b.min[0]) && (a.min[1] <= b.max[1] && a.max[1] >= b.min[1]);
 }
 
-float aabb_ray_distance(vec2 rp, vec2 rd, vec2 bp, vec2 bs)
+float aabb_ray_distance(vec2 rp, vec2 rd, aabb_t a)
 {
-	vec2 min, max;
 	vec4 m;
-
-	for(int i = 0; i < 2; i++) {
-		min[i] = bp[i];
-		max[i] = bp[i] + bs[i];
-	}
 
 	m[0] = -INFINITY;
 	m[1] = INFINITY;
 
 	for (int i = 0; i < 2; i++) {
-		m[2] = (min[i] - rp[i]) / rd[i];
-		m[3] = (max[i] - rp[i]) / rd[i];
+		m[2] = (a.min[i] - rp[i]) / rd[i];
+		m[3] = (a.max[i] - rp[i]) / rd[i];
 
 		if (m[2] > m[3]) {
 			float tmp = m[2];
@@ -73,16 +49,9 @@ float aabb_ray_distance(vec2 rp, vec2 rd, vec2 bp, vec2 bs)
 	return (m[0] > m[1]) ? INFINITY : m[0];
 }
 
-_Bool aabb_ray_intersect(float *distance, vec2 rp, vec2 rd, vec2 bp, vec2 bs)
+_Bool aabb_ray_intersect(float *distance, vec2 rp, vec2 rd, aabb_t a)
 {
-	vec2 min, max;
-
-	for(int i = 0; i < 2; i++) {
-		min[i] = bp[i];
-		max[i] = bp[i] + bs[i];
-	}
-
-	float d = aabb_ray_distance(rp, rd, bp, bs);
+	float d = aabb_ray_distance(rp, rd, a);
 
 	if (d == INFINITY) {
 		return 0;
