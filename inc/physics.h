824248760efd907537806b154618c7163c06a590
tone121 <tone121@cox.net>
2019-02-24 18:36:26 -0700

    Implemented phys_body object handling

--- inc/physics.h
+++ inc/physics.h
@@ -13,7 +13,10 @@ typedef struct phys_body {
 	vec2 velocity;
 } phys_body_t;
 
+void physics_body_add(phys_body_t *body);
+void physics_body_remove(phys_body_t *body);
+void physics_body_fetch(phys_body_t *body);
 phys_body_t *physics_body_init(vec2 pos, vec2 size);
-void physics_body_step(phys_body_t *body, float deltatime);
+void physics_body_compute(phys_body_t *a, phys_body_t *b, float deltatime);
 
 #endif
--- src/physics.c
+++ src/physics.c
@@ -2,6 +2,76 @@
 
 #define GRAVITY 9.8
 
+typedef struct node {
+	struct node *next;
+	int fetched;
+	phys_body_t *body;
+} node_t;
+
+node_t *bodies = NULL;
+static unsigned int bodycount = 0;
+
+void physics_body_add(phys_body_t *body)
+{
+	bodycount++;
+
+	if (!bodies) {
+		bodies = calloc(1, sizeof(node_t));
+		bodies->next = NULL;
+		bodies->body = body;
+		bodies->fetched = 1;
+
+		return;
+	}
+
+	node_t *newbody = calloc(1, sizeof(node_t));
+	newbody->next = bodies;
+	newbody->body = body;
+	bodies = newbody;
+	bodies->fetched = 1;
+}
+
+void physics_body_remove(phys_body_t *body)
+{
+	if (!bodies)
+		return;
+	
+	node_t *curr = bodies;
+	node_t *prev = NULL;
+
+	while (curr != NULL) {
+		if (curr->body == body) {
+			if (prev) {
+				prev->next = curr->next;
+			}
+
+			bodycount--;
+			free(curr);
+			break;
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+}
+
+void physics_body_fetch(phys_body_t *body)
+{
+	if (!bodies)
+		return;
+	
+	node_t *curr = bodies;
+
+	while (curr != NULL) {
+		if (curr->fetched == 1) {
+			*body = *curr->body;
+			curr->fetched = 0;
+			break;
+		}
+
+		curr = curr->next;
+	}
+}
+
 phys_body_t *physics_body_init(vec2 pos, vec2 size)
 {
 	phys_body_t *body = calloc(1, sizeof(phys_body_t));
@@ -17,16 +87,19 @@ phys_body_t *physics_body_init(vec2 pos, vec2 size)
 	return body;
 }
 
-void physics_body_step(phys_body_t *body, float deltatime)
+void physics_body_compute(phys_body_t *a, phys_body_t *b, float deltatime)
 {
 	vec2 force;
 
 	force[0] = 0;
-	force[1] = 9.8;
+	force[1] = -9.8;
+
+	if (aabb_quad_intersect(a->aabb, b->aabb))
+		force[1] = 0;
 
 	for (int i = 0; i < 2; i++)
-		body->velocity[i] += force[i] * (deltatime);
+		a->velocity[i] += force[i] * (deltatime);
 
 	for (int i = 0; i < 2; i++)
-		body->position[i] += body->velocity[i];
+		a->position[i] += a->velocity[i];
 }
